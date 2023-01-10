9847a396ff3b94e4a7100468ebe4ef2ff2b29b06
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-01-10 11:52:40 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let nb1 = Number(prompt("Veuillez saisir un premier nombre"));
+let nb2 = Number(prompt("Veuillez saisir un second nombre"));
+
+let msg = `La somme de ${nb1} et ${nb2} est : ${nb1+nb2}`;
+console.log(msg);
